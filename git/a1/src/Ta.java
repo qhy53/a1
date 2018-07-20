19e1f3af9de6eca51fafb7a9ac435426a1bd87d3
haiyu <1798380447@qq.com>
2018-07-20 18:48:44 +0800

    添加注释并打印y

--- git/a1/src/Ta.java
+++ git/a1/src/Ta.java
@@ -2,7 +2,11 @@ public class Ta {
     // 声明变量x;
     private static int x = 10;
     public static void main(String[] args) {
+        //声明变量y
         int y = 1;
+        //打印x
         System.out.println("此时 x = " + x);
+        //打印y
+        System.out.println(y);
     }
 }
