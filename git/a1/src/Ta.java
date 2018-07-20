1da38c74cf5bc4fa4fb6b321b8506e7859a558ba
haiyu <1798380447@qq.com>
2018-07-20 18:46:27 +0800

    存储

--- git/a1/src/Ta.java
+++ git/a1/src/Ta.java
@@ -2,6 +2,7 @@ public class Ta {
     // 声明变量x;
     private static int x = 10;
     public static void main(String[] args) {
+        int y = 1;
         System.out.println("此时 x = " + x);
     }
 }
